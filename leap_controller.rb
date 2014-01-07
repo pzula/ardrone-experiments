1eb6a53fb47b9a9bc0e17e415c0e7f696e6829d1
Persa Zula <persa@persazula.com>
2014-01-07 15:35:26 -0500

    Ardrone takeoff working with LeapMotion. Need to slow down framerate from LeapMotion

--- leap_controller.rb
+++ leap_controller.rb
@@ -1,39 +1,67 @@
 require 'artoo'
-require 'argus'
+require 'pry'
 
 connection :leapmotion, :adaptor => :leapmotion, :port => '127.0.0.1:6437'
 device :leapmotion, :driver => :leapmotion
 
-drone = Argus::Drone.new
+connection :ardrone, :adaptor => :ardrone
+device :drone, :driver => :ardrone, :connection => :ardrone
 
 work do
+
   on leapmotion, :open => :on_open
   on leapmotion, :frame => :on_frame
-  # on leapmotion, :close => :on_close
+  on leapmotion, :close => :on_close
 end
 
 def on_open(*args)
-  puts "Egadrone is ready for gestures"
+  puts args
 end
 
-def on_frame(*args)
-  frame = args[1]
-  #puts frame
-  frame.hands.each do |hand|
-    puts "x: #{hand.palm_x}, y: #{hand.palm_y}, z: #{hand.palm_z}"
-    fly_forward if forward?(hand)
-    fly_backward if backward?(hand)
-    roll_right if right?(hand)
-    roll_left if left?(hand)
 
-  end
+
+def on_frame(*args)
+  # return if not_ready
+  frame = args[1]
+  # #puts frame
+  # frame.hands.each do |hand|
+  return if frame.hands.empty?
+  hand = frame.hands.first
+    #binding.pry
+    puts "x: #{hand.palm_x}, y: #{hand.palm_y}, z: #{hand.palm_z}"
+    #fly_forward if forward?(hand)
+    #fly_backward if backward?(hand)
+    #roll_right if right?(hand)
+    #roll_left if left?(hand)
+    lift_off if lifted?(hand)
+    landing if lowered?(hand)
+    steady if hover?(hand)
+  # end
+  pause_work
+  sleep 2
+  continue_work
   #puts frame.gestures
   #sleep 3
   #puts frame.pointables
+
+  # drone.start
+  # drone.take_off
+  # drone.hover
+  # sleep 1
+  # drone.land
+  # drone.stop
 end
 
 def on_close(*args)
   puts args
+end
+
+def lifted?(hand)
+  (160..300).include?(hand.palm_z)
+end
+
+def lowered?(hand)
+  (65..145).include?(hand.palm_z)
 end
 
 def hover?(hand)
@@ -41,44 +69,78 @@ def hover?(hand)
   (90..130).include?(hand.palm_y) &&
   (145..160).include?(hand.palm_z)
 end
+
 def forward?(hand)
   (-10..10).include?(hand.palm_x) &&
   (0..90).include?(hand.palm_y) &&
   (145..160).include?(hand.palm_z)
 end
+
 def backward?(hand)
   (-10..10).include?(hand.palm_x) &&
   (130..150).include?(hand.palm_y) &&
   (145..160).include?(hand.palm_z)
 end
+
 def left?(hand)
   (-10..-110).include?(hand.palm_x) &&
   (90..130).include?(hand.palm_y) &&
   (145..160).include?(hand.palm_z)
 end
+
 def right?(hand)
   (10..150).include?(hand.palm_x) &&
   (90..130).include?(hand.palm_y) &&
   (145..160).include?(hand.palm_z)
 end
 
+def steady
+  drone.start
+  drone.take_off
+  drone.hover
+end
+
 def fly_forward
-  drone.forward(0.5)  
+  drone.forward(0.5)
   sleep 2
   drone.hover
 end
+
 def fly_backward
   drone.backward(0.5)
   sleep 2
   drone.hover
 end
+
 def roll_right
   drone.right(0.5)
   sleep 2
   drone.hover
 end
+
 def roll_left
   drone.left(0.5)
   sleep 2
   drone.hover
 end
+
+def lift_off
+  puts 'we have lift off!'
+  drone.start
+  drone.take_off
+end
+
+def landing
+  puts "landing"
+  unless drone.nil?
+    drone.hover
+    sleep 1
+    drone.land
+    drone.stop
+  end
+end
+
+
+
+
+
